3c2b484528260ed17ffccc01db8568ac50d3b5c9
MuhRama07 <ramapradipa37@gmail.com>
2024-05-27 06:11:05 +0700

    update admin

--- app.py
+++ app.py
@@ -1,7 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# BAGIAN ADMIN #
+@app.route('/dashboard')
+def dashboard():
+    return render_template('ad_index.html')
+
+@app.route('/adpesanan')
+def adpesanan():
+    return render_template('ad_pesanan.html')
+
+@app.route('/adproduk')
+def adproduk():
+    return render_template('ad_produk.html')
+
+@app.route('/adpelanggan')
+def adpelanggan():
+    return render_template('ad_pelanggan.html')
+
+@app.route('/adlpenjualan')
+def adlpenjualan():
+    return render_template('ad_lpenjualan.html')
+
+@app.route('/adlproduk')
+def adlproduk():
+    return render_template('ad_lproduk.html')
+
+@app.route('/adpembayaran')
+def adpembayaran():
+    return render_template('ad_lproduk.html')
+
+@app.route('/adpengiriman')
+def adlpengiriman():
+    return render_template('ad_pengiriman.html')
+
+@app.route('/adpengguna')
+def adlpengguna():
+    return render_template('ad_pengguna.html')
+
+@app.route('/adprofil')
+def adprofil():
+    return render_template('ad_profil.html')
+# BAGIAN ADMIN #
+
+
+
+# BAGIAN USER #
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -25,8 +70,9 @@ def pesanan():
 @app.route('/keranjang')
 def keranjang():
     return render_template('keranjang.html')
+# BAGIAN USER #
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000,debug=True)
