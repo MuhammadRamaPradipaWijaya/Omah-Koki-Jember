48475ba9eecfa9d8ce79193d79747be6a329f5ba
MuhRama07 <ramapradipa37@gmail.com>
2024-06-20 19:04:29 +0700

    update

--- app.py
+++ app.py
@@ -81,6 +81,8 @@ def adpesanan():
     else:
         return redirect(url_for('adlogin'))
 
+
+
 @app.route('/update_status/<_id>', methods=['POST'])
 def update_status(_id):
     data = request.get_json()
